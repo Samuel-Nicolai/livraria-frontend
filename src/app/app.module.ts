ed73144c93f3e72e1c314095b968e414fba4cad0
Samuel Nicolai <nicolaisamuel3@gmail.com>
2019-11-05 18:09:21 -0300

    Template inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Material modules imports
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatSidenavModule } from '@angular/material/sidenav'
+
+// Components imports
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
